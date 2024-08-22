8cc0b5df1b8dc75ad9a6210ecd14c8f61b7b0832
saraz <shfloopyuh@gmail.com>
2024-08-22 16:43:50 -0500

    getting closer

--- src/main/java/com/shfloop/simply_shaders/SimplyShaders.java
+++ src/main/java/com/shfloop/simply_shaders/SimplyShaders.java
@@ -35,13 +35,14 @@ public class SimplyShaders implements ModInitializer {
                 1.0f, -1.0f,  1.0f, 0.0f,
                 1.0f,  1.0f,  1.0f, 1.0f
         };
+        FloatBuffer
          int quadVAO = GL32.glGenVertexArrays();
         int quadVBO = GL32.glGenBuffers();
        GL32.glBindVertexArray(quadVAO);
         GL32.glBindBuffer(GL32.GL_ARRAY_BUFFER, quadVBO);
         GL32.glBufferData(GL32.GL_ARRAY_BUFFER,quadVertices, GL32.GL_STATIC_DRAW);
         GL32.glEnableVertexAttribArray(0);
-        GL32.glVertexAttribPointer(0, 2, GL32.GL_FLOAT, GL32.GL_FALSE, 4 * sizeof(float), (void*)0);
+        GL32.glVertexAttribPointer(0, 2, GL32.GL_FLOAT, GL32.GL_FALSE);
         GL32.glEnableVertexAttribArray(1);
         GL32.glVertexAttribPointer(1, 2, GL32.GL_FLOAT, GL32.GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
         GL32.glBindVertexArray(0);
