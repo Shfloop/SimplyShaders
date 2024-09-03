00bd6d12fa3382602cf6be07540dca57497681e2
Ben's PC <shfloopyuh@gmail.com>
2024-09-03 17:15:26 -0500

    time fix

---
+++ src/main/java/com/shfloop/simply_shaders/DynamicSkyInterface.java
@@ -0,0 +1,5 @@
+package com.shfloop.simply_shaders;
+
+public interface DynamicSkyInterface {
+     void setLastUpdateTime(float time);
+}
---
+++ src/main/java/com/shfloop/simply_shaders/mixins/CommandTimeMixin.java
@@ -0,0 +1,23 @@
+package com.shfloop.simply_shaders.mixins;
+
+import com.shfloop.simply_shaders.DynamicSkyInterface;
+import finalforeach.cosmicreach.chat.commands.CommandTime;
+import finalforeach.cosmicreach.world.Sky;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CommandTime.class)
+public class CommandTimeMixin  {
+    @Inject(method = "run", at = @At("TAIL"))
+    private void updateDynamicSkySunUpdate(CallbackInfo ci) {
+        if(Sky.currentSky.skyId.equals("base:dynamic_sky")) {
+            ((DynamicSkyInterface) Sky.currentSky).setLastUpdateTime(0);
+            System.out.println("SETTING LAST UPDATE");
+        }
+
+    }
+
+
+}
